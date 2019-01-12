c8fd2ca830dd7f93b4f3819e012555aec837ad18
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2019-01-11 16:44:06 -0800

    add Deconstructor in refactored_stack

--- stacks/stack_refactored.cpp
+++ stacks/stack_refactored.cpp
@@ -27,6 +27,7 @@ public:
   T pop();
   T printStack();
   T getItemAt(int index);
+  bool isEmpty();
 };
 
 template <class T>
@@ -63,28 +64,37 @@ T Stack<T>::getItemAt(int index){
 
   return -1;
 }
+
+template <class T>
+bool Stack<T>::isEmpty(){
+  if (rear <= 0) {
+    return true;
+  }
+
+  return false;
+}
 // End Class
 
 
 int main(void){
-    Stack<int> myStack(10);
-    myStack.insert(1);
-    myStack.insert(2);
-    myStack.insert(3);
-    myStack.insert(4);
-    myStack.printStack();
-    cout << myStack.getItemAt(4) << endl;
-    cout << myStack.getItemAt(1) << "\n\n" << endl;
-    int someInt = {myStack.pop()};
-    int nextInt = {myStack.pop()};
+    Stack<int> *myStack = new Stack<int>(10);
+    myStack->insert(1);
+    myStack->insert(2);
+    myStack->insert(3);
+    myStack->insert(4);
+    myStack->printStack();
+    cout << myStack->getItemAt(4) << endl;
+    cout << myStack->getItemAt(1) << "\n\n" << endl;
+    int someInt = {myStack->pop()};
+    int nextInt = {myStack->pop()};
     cout << "INT RETURNED IS: " << someInt << endl;
     cout << "NEXT INT IS: " << nextInt << endl;
-    cout << "AND UP NEXT: " << myStack.pop() << endl;
-    cout << "AND UP NEXT: " << myStack.pop() << endl;
+    cout << "AND UP NEXT: " << myStack->pop() << endl;
+    cout << "AND UP NEXT: " << myStack->pop() << endl;
     cout << "\nHERE ON AFTER, SHOULD ONLY RETURN -1 FOR ERROR / EMPTY STACK" << endl;
-    cout << "AND UP NEXT: " << myStack.pop() << endl;
-    cout << "AND UP NEXT: " << myStack.pop() << endl;
-    cout << "AND UP NEXT: " << myStack.pop() << endl;
-    myStack.~Stack();
+    cout << "AND UP NEXT: " << myStack->pop() << endl;
+    cout << "AND UP NEXT: " << myStack->pop() << endl;
+    cout << "AND UP NEXT: " << myStack->pop() << endl;
+    myStack->~Stack();
     return 0;
 }
