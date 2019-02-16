d159677d88b0d8824ad392659551136472a0dbfc
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2019-02-15 17:09:24 -0800

    add stack tests

--- stacks/stack_test.cpp
+++ stacks/stack_test.cpp
@@ -1,5 +1,5 @@
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE Simple testcases 5
+#define BOOST_TEST_MODULE Stack testcases 5
 #include <boost/test/unit_test.hpp>
 #include <boost/assert.hpp>
 #include <boost/test/execution_monitor.hpp>
